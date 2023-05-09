f016827abb059f86b17159f30a3045091a66dd94
Archit Dharood <archit.dharod@pflanzmich.de>
2023-05-09 10:02:06 +0000

    added config file for env variables

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,39 @@
 // TODO: Setup Docker
 // TODO: logging
-// TODO: project specific perttierrc
+
+// modules import
+import express from 'express';
+import * as http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import Debug from 'debug';
+
+// variables import
+import envVar from './config';
+
+// Debug configuration
+const debug: Debug.IDebugger = Debug('app.ts');
+
+// SIGINT for Killing Docker container
+const process = require('process');
+
+process.on('SIGINT', () => {
+  debug('SIGINT event detected. Exiting...');
+  process.exit(0);
+});
+
+// App configuration
+const app = express();
+const server: http.Server = http.createServer(app);
+app.use(helmet);
+app.use(cors());
+
+// Test endpoint
+app.get('/', (req, res) => {
+  res.status(200).send('This is working');
+});
+
+// Server activation
+server.listen(envVar.port, () => {
+  debug(`server is listning on port: ${envVar.port}`);
+});
---
+++ src/config/index.ts
@@ -0,0 +1,39 @@
+import * as dotenv from 'dotenv';
+import Debug from 'debug';
+
+// Debug configuration
+const debug: Debug.IDebugger = Debug('config/index.ts');
+
+// specify the path of the .env file based on the NODE_ENV
+let envPath;
+const nodeEnv = process.env.NODE_ENV;
+if (nodeEnv === 'test') {
+  envPath = '.env.test';
+} else if (nodeEnv === 'production') {
+  envPath = '.env.production';
+} else {
+  envPath = '.env';
+}
+
+dotenv.config({ path: envPath });
+
+// Define your environment variables
+const envVar = {
+  port: <string>process.env.PORT,
+  // ... add more variables as needed
+};
+
+export default envVar;
+
+// Utility function to check if all environment variables exist
+function checkEnvVariables(variables: Record<string, string | undefined>) {
+  Object.entries(variables).forEach(([key, value]) => {
+    if (typeof value === 'undefined' || value === null) {
+      debug(`"${key}" environment variable is not defined`);
+      process.exit(1); // Exit the process with a "failure" code
+    }
+  });
+}
+
+// Check all environment variables
+checkEnvVariables(envVar);
